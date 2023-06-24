e4af589ed9515818a6064a7dd215f4b63016f8f8
Otávio <otavio@cargon.com.br>
2023-06-23 21:55:30 -0300

    string perm done

--- algorithms/string_permutations.js
+++ algorithms/string_permutations.js
@@ -2,26 +2,24 @@ function permutation(string) {
   const splited = string.split("");
 
   function print(current, total) {
-    console.log('entrei     ');
+    // console.log('entrei');
     if (current.length === 0) {
       return;
     }
     if (current.length === 1) {
-      // console.log(total + current[0]);
+      console.log(total + current[0]);
       return;
     }
     let right;
     let left;
-    for (i = 0; i < current.length; i++) {
+    for (let i = 0; i < current.length; i++) {
       left = current.slice(0, i);
       right = current.slice(i + 1, current.length);
-      print(left, total + current[i]);
-      print(right, total + current[i]);
-      // console.log(current, i);
+      print([...left, ...right], total + current[i]);
     }
   }
 
   print(splited, "");
 }
 
-permutation("abc");
+permutation("abcd");
